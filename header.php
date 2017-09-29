e307cfbd591b499444b39ab7bfde4b76f1ac665a
Chow-boy <jeanin.moussa@gmail.com>
2017-09-29 11:10:58 +0200

    new save

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <header>
   <nav id ="navigation">
-    <a href="#index"><i class="fa fa-home fa-2x" aria-hidden="true"></i>Home</a>
-    <a href="#talk"><i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>About me</a>
-    <a href="#work"><i class="fa fa-folder-open fa-2x" aria-hidden="true"></i>Work</a>
-    <a href="#contact"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i>Contact</a>
+    <a id="accueil" class="meoh" href="#index"><i class="fa fa-home fa-2x" aria-hidden="true"></i>Home</a>
+    <a id="moi" href="#talk"><i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>About me</a>
+    <a id="travail" href="#work"><i class="fa fa-folder-open fa-2x" aria-hidden="true"></i>Work</a>
+    <a id="client" href="#contact"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i>Contact</a>
     <a href="cv.php"><i class="fa fa-file-text fa-2x" aria-hidden="true"></i>CV</a>
   </nav>
 </header>
--- work.php
+++ work.php
@@ -8,31 +8,31 @@
   La deuxiéme ligne, c'est du développement avec du PHP et Javascript. </p>-->
       <div class="">
 
-      </div id="divs">
-      <div>
+      </div>
+      <div class="box">
       <a href="Green Office/index.html"><img id="greenoffice" class="rectangle" src="image/maquette/img8.jpg" alt="maquette"></a>
       </div>
-      <div>
+      <div class="box">
         <a href="Creasoul/index.html"><img id="creasoul"class="rectangle"  src="image/maquette/creasoul.png" alt="maquette"></a>
       </div>
-      <div>
+      <div class="box">
         <a href="Wolf Gang/index.html"><img id="wolfgang" class="rectangle" src="image/maquette/img7.png" alt="maquette"></a>
       </div>
-      <div>
+      <div class="box">
         <a href="Ila Yoga 1/index.html"><img id="yoga" class="rectangle" src="image/maquette/img6.jpg" alt="maquette"></a>
       </div>
 
 
-      <div>
+      <div class="box">
         <a href="Carnet d'addresse/address-book.html"><img id="carnet" class="rectangle" src="image/carnet.png" alt="image"></a>
       </div>
-      <div>
+      <div class="box">
         <a href="Gestions de la souris/index.html"><img id="souris" class="rectangle" src="image/souris.png" alt="image"></a>
       </div>
-      <div>
+      <div class="box">
         <a href="Bons_de_commande/index.phtml"><img id="bons" class="rectangle" src="image/bons.png" alt="image"></a>
       </div>
-      <div>
+      <div class="box">
         <a href="blog/layout.phtml"><img id="blog" class="rectangle" src="image/blog.png" alt="image"></a>
       </div>
 
