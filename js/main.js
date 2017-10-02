af636d774d63f414fc70b2ced4ffbb1ebd97eca7
Chow-boy <jeanin.moussa@gmail.com>
2017-10-02 13:28:55 +0200

    new save

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 "use strict";
 
 $('#work').mouseenter(function() {
-    $("#box1").animate({marginLeft:'25px', marginTop:'5px'}, 1000);
-    $("#box2").animate({marginLeft:'15px', marginTop:'5px'}, 2000);
-    $("#box3").animate({marginLeft:'15px', marginTop:'5px'}, 3000);
-    $("#box4").animate({marginLeft:'15px', marginTop:'5px'}, 4000);
-    $("#box5").animate({marginLeft:'25px', marginTop:'10px'}, 1000);
-    $("#box6").animate({marginLeft:'15px', marginTop:'10px'}, 2000);
-    $("#box7").animate({marginLeft:'15px', marginTop:'10px'}, 3000);
-    $("#box8").animate({marginLeft:'10px', marginTop:'10px'}, 4000);
-
+    $("#box1").animate({marginLeft:'50px', marginTop:'200px'}, 1000);
+    $("#box2").animate({marginLeft:'270px', marginTop:'200px'}, 2000);
+    $("#box3").animate({marginLeft:'490px', marginTop:'200px'}, 3000);
+    $("#box4").animate({marginLeft:'710px', marginTop:'200px'}, 4000);
+    $("#box5").animate({marginLeft:'50px', marginTop:'500px'}, 1000);
+    $("#box6").animate({marginLeft:'270px', marginTop:'500px'}, 2000);
+    $("#box7").animate({marginLeft:'490px', marginTop:'500px'}, 3000);
+    $("#box8").animate({marginLeft:'710px', marginTop:'500px'}, 4000);
 
 });
