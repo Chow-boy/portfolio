ad2fd90062557da68384478709a268976dcbc773
Chow-boy <jeanin.moussa@gmail.com>
2017-10-09 14:04:59 +0200

    sauvegarde

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         <a href="#talk"><i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>About me</a>
         <a href="#work"><i class="fa fa-folder-open fa-2x" aria-hidden="true"></i>Work</a>
         <a href="#contact"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i>Contact</a>
-        <a href="cv.php"><i class="fa fa-file-text fa-2x" aria-hidden="true"></i>CV</a>
+        <a href="cv.html"><i class="fa fa-file-text fa-2x" aria-hidden="true"></i>CV</a>
       </nav>
     </header>
     <main>
